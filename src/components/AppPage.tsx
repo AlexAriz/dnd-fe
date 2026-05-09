b8c3b50b2b2abf3c3646ca5c5c932f9da5cc7f6d
Alex <alex.ariztegui@gmail.com>
2026-05-09 16:13:47 -0600

    test

--- src/components/AppPage.tsx
+++ src/components/AppPage.tsx
@@ -7,6 +7,7 @@ import useLanguage from "../hooks/useLanguage";
 import Auth from "../auth";
 import Dummy from "./Dummy";
 import Welcome from "./Welcome";
+import { Typography } from "@mui/joy";
 
 function AppPage() {
   const navigate = useNavigate();
@@ -26,7 +27,10 @@ function AppPage() {
 
   return (
     <>
-      {import.meta.env.SUPABASE_URL}
+      <Typography>
+        Test - {import.meta.env.SUPABASE_URL} - {import.meta.env.VITE_SUPABASE_URL}
+      </Typography>
+
       {Object.values(LANGUAGES).map((language) => (
         <Button key={language} variant={locale === language ? "solid" : "soft"} onClick={() => changeLocale(language)}>
           {language}
