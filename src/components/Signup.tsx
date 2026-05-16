b790d853a9f30a31ae3316ddb1aebccc8ed63f34
Alex <alex.ariztegui@gmail.com>
2026-05-15 18:37:46 -0600

    fix mobile width

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -35,7 +35,7 @@ function Signup() {
 
   return (
     <div className="flex flex-col h-lvh w-lvw items-center-safe justify-center-safe">
-      <Stack component="form" onSubmit={handleSubmit} className="w-lg space-y-4">
+      <Stack component="form" onSubmit={handleSubmit} className="w-1/2 md:w-lg space-y-4">
         <Typography variant="h3">{intl.get("SIGNUP")}</Typography>
 
         <TextField
