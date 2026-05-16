8b26f3b245f82e106bdf06b6c23967c71faef718
Alex <alex.ariztegui@gmail.com>
2026-05-16 17:40:21 -0600

    update language picker

--- src/components/LanguagePicker.tsx
+++ src/components/LanguagePicker.tsx
@@ -1,39 +1,26 @@
-import MenuItem from "@mui/material/MenuItem";
-import Select, { type SelectChangeEvent } from "@mui/material/Select";
 import { LANGUAGES } from "Constants/language";
 import type { Languages } from "Types/language";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import { useIntl } from "react-intl";
 import { useAppDispatch, useAppSelector } from "Hooks/state";
 import { languageActions, languageSelectors } from "State/language";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import ToggleButton from "@mui/material/ToggleButton";
 
 function LanguagePicker() {
   const dispatch = useAppDispatch();
   const language = useAppSelector(languageSelectors.selectLanguage);
-  const intl = useIntl();
 
-  const handleChange = (event: SelectChangeEvent) => {
-    const selectedLanguage = event.target.value as Languages;
+  const handleChange = (_event: React.MouseEvent<HTMLElement>, selectedLanguage: Languages) => {
     dispatch(languageActions.changeLanguage(selectedLanguage));
   };
 
   return (
-    <FormControl size="small">
-      <InputLabel id="language-picker-label">{intl.formatMessage({ id: "LANGUAGE" })}</InputLabel>
-      <Select
-        labelId="language-picker-label"
-        label={intl.formatMessage({ id: "LANGUAGE" })}
-        value={language}
-        onChange={handleChange}
-      >
-        {Object.values(LANGUAGES).map((language) => (
-          <MenuItem key={language} value={language}>
-            {language}
-          </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
+    <ToggleButtonGroup value={language} exclusive onChange={handleChange}>
+      {Object.values(LANGUAGES).map((language) => (
+        <ToggleButton key={language} value={language}>
+          {language}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
   );
 }
 export default LanguagePicker;
