db598f147ecda002b1f762e6002b906ba2498be7
Alex <alex.ariztegui@gmail.com>
2026-05-18 15:41:08 -0600

    update theme icons

--- src/core/constants/themes.ts
+++ src/core/constants/themes.ts
@@ -1,6 +1,6 @@
 import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
-import BuildCircleIcon from "@mui/icons-material/BuildCircle";
+import BrightnessMediumIcon from "@mui/icons-material/BrightnessMedium";
 import type { Theme } from "../types/theme";
 
 export const THEMES: Readonly<Record<string, Theme>> = {
@@ -8,12 +8,12 @@ export const THEMES: Readonly<Record<string, Theme>> = {
     value: "light",
     Icon: LightModeIcon,
   },
+  SYSTEM: {
+    value: "system",
+    Icon: BrightnessMediumIcon,
+  },
   DARK: {
     value: "dark",
     Icon: DarkModeIcon,
-  },
-  SYSTEM: {
-    value: "system",
-    Icon: BuildCircleIcon,
   },
 };
