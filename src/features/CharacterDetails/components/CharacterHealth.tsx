2cbcce518c7b9d38a188a5dff27e3d28cf718f09
Alex <alex.ariztegui@gmail.com>
2026-06-18 19:08:05 -0600

    update hitpoints

--- src/features/CharacterDetails/components/CharacterHealth.tsx
+++ src/features/CharacterDetails/components/CharacterHealth.tsx
@@ -6,22 +6,55 @@ import NumberField from "Components/NumberField";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import { useIntl } from "react-intl";
+import { useState } from "react";
+import { usePatchCharacterMutation } from "State/Character";
 
 function CharacterHealth() {
   const intl = useIntl();
+  const [change, setChange] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const character = useRequiredContext(ActiveCharacterContext);
+  const [patchCharacter] = usePatchCharacterMutation();
+
+  const handleChange = async () => {
+    setIsLoading(true);
+    await patchCharacter({ characterId: character.id, hitpoints: change });
+    setChange(0);
+    setIsLoading(false);
+  };
 
   return (
     <Grid container columns={3} rowSpacing={0} columnSpacing={2}>
       <Grid size={1}>
         <Stack>
-          <Button variant="outlined" size="small" color="success">
+          <Button
+            variant="contained"
+            size="small"
+            color="success"
+            onClick={handleChange}
+            loading={isLoading}
+            disabled={change <= 0}
+          >
             {intl.formatMessage({ id: "HEAL" })}
           </Button>
 
-          <NumberField size="small" />
+          <NumberField
+            size="small"
+            value={change}
+            onValueChange={(value) => setChange(value ?? 0)}
+            max={character.hitPoints.max - character.hitPoints.current}
+            min={0 - character.hitPoints.current}
+            step={1}
+          />
 
-          <Button variant="outlined" size="small" color="error">
+          <Button
+            variant="contained"
+            size="small"
+            color="error"
+            onClick={handleChange}
+            loading={isLoading}
+            disabled={change >= 0}
+          >
             {intl.formatMessage({ id: "DAMAGE" })}
           </Button>
         </Stack>
