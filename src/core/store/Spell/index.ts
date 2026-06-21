8df38a4521670b92162bd1c392881e2eb9553c04
Alex <alex.ariztegui@gmail.com>
2026-06-21 17:42:19 -0600

    undo spell markdown update

--- src/core/store/Spell/index.ts
+++ src/core/store/Spell/index.ts
@@ -14,23 +14,6 @@ const spellApi = createApi({
     }),
     getSpell: build.query<SpellDetail, string>({
       query: (spellId) => `spells/${spellId}`,
-      transformResponse: (spell: SpellDetail) => {
-        const mdParts: string[] = [spell.markdown];
-
-        if (spell.classes.length > 0) {
-          const classes = spell.classes.map((_class) => _class.name);
-          mdParts.push(`**Classes**: ${classes.join(", ")}`);
-        }
-        if (spell.subClasses.length > 0) {
-          const subclasses = spell.subClasses.map((subclass) => `_${subclass.shortName}_ ${subclass.class.name}`);
-          mdParts.push(`**Subclasses**: ${subclasses.join(", ")}`);
-        }
-
-        return {
-          ...spell,
-          markdown: mdParts.join("\n\n"),
-        };
-      },
       providesTags: (_result, _error, id) => [{ type: "Spell", id }],
     }),
   }),
