3838b8934ba3b99452a74111f5f64318a01472a3
Alex <alex.ariztegui@gmail.com>
2026-06-26 15:49:08 -0600

    fix spell details height

--- src/features/SpellList/views/SpellList.tsx
+++ src/features/SpellList/views/SpellList.tsx
@@ -17,7 +17,7 @@ function SpellList() {
         <>
           <Divider sx={{ display: { xs: "block", md: "none" } }} />
           <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" } }} />
-          <div className="w-full md:w-1/3 max-h-1/2 md:max-h-full overflow-auto mb-3 md:mb-0 md:ml-3">
+          <div className="w-full md:w-1/3 h-1/2 md:h-full overflow-auto mb-3 md:mb-0 md:ml-3">
             <SpellDetails spellId={spellId} />
           </div>
         </>
