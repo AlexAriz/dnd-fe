52743d21350c0df9be01558c48058f28d11cbf74
Alex <alex.ariztegui@gmail.com>
2026-07-01 20:02:34 -0600

    remove commented code

--- src/features/Profile/views/CompleteProfile.tsx
+++ src/features/Profile/views/CompleteProfile.tsx
@@ -70,30 +70,6 @@ function CompleteProfile() {
       </form>
     </Stack>
   );
-
-  /* return (
-    <>
-      <Typography variant="h3">{intl.formatMessage({ id: "MODULE_PROFILE" })}</Typography>
-
-      <Stack className="flex flex-col px-4 py-8 w-1/2 md:w-lg space-y-4" component="form" onSubmit={onSubmit}>
-        <TextField
-          label={intl.formatMessage({ id: "USERNAME" })}
-          placeholder={intl.formatMessage({ id: "USERNAME" })}
-          disabled={Boolean(profile?.username)}
-          defaultValue={profile?.username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-
-        <Button type="submit" variant="contained" disabled={!username} loading={loading}>
-          {intl.formatMessage({ id: "SAVE" })}
-        </Button>
-      </Stack>
-
-      <Toast isOpen={snackbarOpen} onClose={() => setSnackbarOpen(false)} severity="error">
-        {intl.formatMessage({ id: "CREATE_PROFILE_ERROR" })}
-      </Toast>
-    </>
-  ); */
 }
 
 export default CompleteProfile;
