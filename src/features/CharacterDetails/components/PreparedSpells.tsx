257cdc84346f4251f982fecce1c13446afbe1c90
Alex <alex.ariztegui@gmail.com>
2026-07-04 13:21:34 -0600

    revamp known spells

--- src/features/CharacterDetails/components/PreparedSpells.tsx
+++ src/features/CharacterDetails/components/PreparedSpells.tsx
@@ -1,5 +1,4 @@
 import { Collapsible, CollapsibleGroup } from "@astryxdesign/core/Collapsible";
-import { Grid } from "@astryxdesign/core/Grid";
 import { Card } from "@astryxdesign/core/Card";
 import useRequiredContext from "Hooks/useRequiredContext";
 import ActiveCharacterContext from "../context/ActiveCharacterContext";
@@ -35,40 +34,27 @@ function PreparedSpells() {
 
   return (
     <CollapsibleGroup type="multiple" defaultValue={Object.keys(spellHash)}>
-      <Stack>
-        {Object.entries(spellHash).map(([level, spells]) => (
-          <Card key={level}>
-            <Collapsible trigger={intl.formatMessage({ id: "SPELL_DETAIL_LEVEL" }, { level })} value={level}>
-              <Grid>
+      <Card>
+        <Stack gap={3}>
+          {Object.entries(spellHash).map(([level, spells]) => (
+            <Collapsible
+              key={level}
+              trigger={intl.formatMessage({ id: "SPELL_DETAIL_LEVEL" }, { level })}
+              value={level}
+            >
+              <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-x-4 space-y-4">
                 {spells.map((spell) => (
-                  <Card key={spell.id}>
+                  <Card key={spell.id} className="inline-block">
                     <SpellCard spellDetail={spell.markdown} />
                   </Card>
                 ))}
-              </Grid>
+              </div>
             </Collapsible>
-          </Card>
-        ))}
-      </Stack>
+          ))}
+        </Stack>
+      </Card>
     </CollapsibleGroup>
   );
-
-  // return Object.entries(spellHash).map(([level, spells]) => (
-  //   <Accordion key={level} defaultExpanded disableGutters>
-  //     <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-  //       {intl.formatMessage({ id: "SPELL_DETAIL_LEVEL" }, { level })}
-  //     </AccordionSummary>
-
-  //     <AccordionDetails>
-  //       <Masonry columns={{ xs: 2, md: 3, lg: 4 }} sequential>
-  //         {spells.map((spell) => (
-  //           <Paper key={spell.id} className="p-2">
-  //             <SpellCard spellDetail={spell.markdown} />
-  //           </Paper>
-  //         ))}
-  //       </Masonry>
-  //     </AccordionDetails>
-  //   </Accordion>
-  // ));
 }
+
 export default PreparedSpells;
