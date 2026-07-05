25def92aa866d0bf7dc6dc9f17a09b7bd0653c65
Alex <alex.ariztegui@gmail.com>
2026-07-05 11:52:46 -0600

    use stacks for character abilities

--- src/features/CharacterDetails/components/AbilityStat.tsx
+++ src/features/CharacterDetails/components/AbilityStat.tsx
@@ -4,8 +4,9 @@ import type { StatSummary } from "State/Stats/type";
 import ActiveCharacterContext from "../context/ActiveCharacterContext";
 import { useIntl } from "react-intl";
 import { Card } from "@astryxdesign/core/Card";
-import { Layout, LayoutFooter, LayoutHeader, LayoutPanel, Stack } from "@astryxdesign/core/Layout";
+import { Stack, StackItem } from "@astryxdesign/core/Layout";
 import { Text } from "@astryxdesign/core/Text";
+import { Divider } from "@astryxdesign/core/Divider";
 
 interface AbilityStatProps {
   stat: StatSummary;
@@ -17,38 +18,43 @@ function AbilityStat({ stat }: AbilityStatProps) {
 
   return (
     <Card>
-      <Layout
-        header={
-          <LayoutHeader hasDivider className="text-center">
-            <Text>{stat.name}</Text>
-          </LayoutHeader>
-        }
-        start={
-          <LayoutPanel width="50%" hasDivider>
-            <Stack align="end">
-              <Text type="supporting">{intl.formatMessage({ id: "ABILITY_CHECK" })}</Text>
-              <Text type="large">
-                {intl.formatNumber(getAbilityCheck(character, stat.id), { signDisplay: "exceptZero" })}
-              </Text>
-            </Stack>
-          </LayoutPanel>
-        }
-        end={
-          <LayoutPanel width="50%">
-            <Stack align="start">
-              <Text type="supporting">{intl.formatMessage({ id: "ABILITY_SAVE" })}</Text>
-              <Text type="large">
-                {intl.formatNumber(getAbilitySave(character, stat.id), { signDisplay: "exceptZero" })}
-              </Text>
-            </Stack>
-          </LayoutPanel>
-        }
-        footer={
-          <LayoutFooter hasDivider className="text-center">
-            <Text>{character.stats[stat.id].value}</Text>
-          </LayoutFooter>
-        }
-      />
+      <Stack direction="vertical" align="center" gap={2}>
+        <StackItem>
+          <Text>{stat.name}</Text>
+        </StackItem>
+
+        <Divider isFullBleed orientation="horizontal" />
+
+        <StackItem>
+          <Stack direction="horizontal" gap={4}>
+            <StackItem size="fill">
+              <Stack align="end">
+                <Text type="supporting">{intl.formatMessage({ id: "ABILITY_CHECK" })}</Text>
+                <Text type="large">
+                  {intl.formatNumber(getAbilityCheck(character, stat.id), { signDisplay: "exceptZero" })}
+                </Text>
+              </Stack>
+            </StackItem>
+
+            <Divider isFullBleed orientation="vertical" className="self-stretch h-auto" />
+
+            <StackItem size="fill">
+              <Stack align="start">
+                <Text type="supporting">{intl.formatMessage({ id: "ABILITY_SAVE" })}</Text>
+                <Text type="large">
+                  {intl.formatNumber(getAbilitySave(character, stat.id), { signDisplay: "exceptZero" })}
+                </Text>
+              </Stack>
+            </StackItem>
+          </Stack>
+        </StackItem>
+
+        <Divider isFullBleed orientation="horizontal" />
+
+        <StackItem>
+          <Text>{character.stats[stat.id].value}</Text>
+        </StackItem>
+      </Stack>
     </Card>
   );
 }
